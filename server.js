d3ffd60a63c6fb79c9ca7c9127ac6845b7c3fbf6
Sparsh Jain <sparshjain123@gmail.com>
2018-06-07 19:06:17 +0530

    #1 Added chat across nodes feature (via redis).

--- server.js
+++ server.js
@@ -1,28 +1,83 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var port = process.env.PORT || 3000;
 var io = require('socket.io')(http);
+var redis = require("redis");
+var chatServer = {
+  'host' : 'localhost',
+  'port' : '6379'
+}
+var pubClient = redis.createClient(chatServer);
+var subClient = redis.createClient(chatServer);
+subClient.subscribe("chatRadio");
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+//defaults
+var userNick = "Annonymous"
+var connections = 0;
+
+//middlewares
+var checkLoggedIn = function(req, res, next) {
+  if(false) {
+    next();
+  } else {
+    res.sendFile(__dirname + '/login.html');
+  }
+}
+
+app.use(checkLoggedIn);
 
 io.on('connection', function(socket){
-  socket.broadcast.emit('user_join', socket.id);
-
-  socket.on('chat message', function(data){
-    data = JSON.parse(data);
-    io.emit('chat message', {'msg' : data.msg, 'user_id' : data.user_id});
+  socket.on('chat_message', function(data){
+    console.log("chat_message:", data);
+    data['event'] = 'chat_message';
+    pubClient.publish("chatRadio", JSON.stringify(data));
   });
 
   socket.on('disconnect', function() {
-    socket.broadcast.emit('user_left', socket.id);
+    var data = {'event' : 'user_left', 'user' : userNick};
+    console.log("disconnect:", data);
+    pubClient.publish("chatRadio", JSON.stringify(data));
   })
 
   socket.on('nick_change', function(data) {
-    socket.broadcast.emit('nick_change', data)
+    console.log("nick_change:", data);
+    userNick = data.new_nick;
+    if(data["old_nick"].length > 0) {
+      data['event'] = 'nick_change';
+      pubClient.publish("chatRadio", JSON.stringify(data));
+    } else {
+      data['event'] = 'new_user';
+      pubClient.publish("chatRadio", JSON.stringify(data));
+    }
+  })
+
+  subClient.on('message', function(channel, data) {
+    data = JSON.parse(data);
+    console.log('message data:', data);
+    switch (data.event) {
+      case "chat_message":
+        socket.emit("chat_message", {'msg' : data.msg, 'user' : data.user});
+        break;
+
+      case "user_left":
+        socket.emit("user_left", {'user' : data.user});
+        break;
+
+      case "nick_change":
+        socket.emit("nick_change", data);
+        break;
+
+      case "new_user":
+        socket.emit("new_user", data);
+        break;
+    }
   })
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/login.html');
+});
+
+http.listen(port, function(){
+  console.log('listening on *:', port);
 });
